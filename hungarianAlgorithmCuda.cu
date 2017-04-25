00b49538b731da6ecc0376d94cf0bd6893360a56
bodyo <djei0967@gmail.com>
2017-04-25 23:57:08 +0300

    Started rewriting from c++ to CUDA

--- hungarianAlgorithmCuda.cu
+++ hungarianAlgorithmCuda.cu
@@ -1,41 +1,44 @@
-#include <vector>
-#include <limits>
+// #include <vector>
+// #include <limits>
 #include <iostream>
+#include <thrust/device_vector.h>
+#include <thrust/pair.h>
+#include <thrust/device_reference.h>
+// #include <thrust/
+// #include <thrust/
+
+
 using namespace std;
 
-typedef pair<int, int> PInt;
-typedef vector<int> VInt;
-typedef vector<VInt> VVInt;
-typedef vector<PInt> VPInt;
+typedef thrust::pair<int, int> PInt;
+typedef thrust::device_vector<int> VInt;
+typedef thrust::device_vector<VInt> VVInt;
+typedef thrust::device_vector<PInt> VPInt;
 
 const int inf = numeric_limits<int>::max();
 
-VPInt hungarian(const VVInt &matrix) {
-   
-   
-   int height = matrix.size(), width = matrix[0].size();
-   
-   
+__global__
+void hungarian(VVInt *matrix, VPInt *result) 
+{
+   int height = matrix->size(), width = (*matrix)[0]->size();
    VInt u(height, 0), v(width, 0);
-   
-   
    VInt markIndices(width, -1);
-   
    
    for(int i = 0; i < height; i++) {
       VInt links(width, -1);
       VInt mins(width, inf);
       VInt visited(width, 0);
       
-      
       int markedI = i, markedJ = -1, j;
       while(markedI != -1) {
          
          j = -1;
          for(int j1 = 0; j1 < width; j1++)
-            if(!visited[j1]) {
-               if(matrix[markedI][j1] - u[markedI] - v[j1] < mins[j1]) {
-                  mins[j1] = matrix[markedI][j1] - u[markedI] - v[j1];
+            if(!visited[j1]) 
+            {
+               if((*matrix)[markedI][j1] - u[markedI] - v[j1] < mins[j1]) 
+               {
+                  mins[j1] = (*matrix)[markedI][j1] - u[markedI] - v[j1];
                   links[j1] = markedJ;
                }
                if(j==-1 || mins[j1] < mins[j])
@@ -52,23 +55,19 @@ VPInt hungarian(const VVInt &matrix) {
             }
          u[i] += delta;
          
-         
          visited[j] = 1;
          markedJ = j;
          markedI = markIndices[j];   
       }
       
-      
       for(; links[j] != -1; j = links[j])
          markIndices[j] = markIndices[links[j]];
       markIndices[j] = i;
    }
-   
-   VPInt result;
+
    for(int j = 0; j < width; j++)
       if(markIndices[j] != -1)
-         result.push_back(PInt(markIndices[j], j));
-   return result;
+         result->push_back(PInt(markIndices[j], j));
 }
 
 int main(){
@@ -95,31 +94,35 @@ int main(){
     	for(int j = 0; j < lengthOfRow; ++j)
     		cin >> matrixOfEmployeeWork[i][j];
 
-   VVInt matrix;
-   for(int i=0; i<lengthOfColumn; i++)
-   {
-    	VInt row;
-     	for(int j=0; j<lengthOfRow; j++) 
-     		row.push_back(matrixOfEmployeeWork[i][j]);  
-     	matrix.push_back(row);
-   }
-   cout << "debug" << endl;
-   VPInt result;
-   long t1 = clock();
-   for(int i=0; i < 10000; i++)
-   	result = hungarian(matrix);
+   	VVInt *matrix = new VVInt;
+   	for(int i=0; i<lengthOfColumn; i++)
+   	{
+   	 	VInt row;
+   	  	for(int j=0; j<lengthOfRow; j++) 
+   	  		row.push_back(matrixOfEmployeeWork[i][j]);  
+   	  	matrix->push_back(row);
+   	}
 
-   t1 = clock() - t1;
+   	VPInt *result = new VPInt;
+   	long t1 = clock();
 
-   int cost = 0;
-   for(int i = 0; i < result.size(); i++)
-   {
-    	pair <int,int> e = result[i];
-        cout << e.first << "->" << e.second << endl;
+   	// Run kernel on 1M elements on the GPU
+  	hungarian<<<1, 1>>>(matrix, result);
+
+  	// Wait for GPU to finish before accessing on host
+  	cudaDeviceSynchronize();
+		
+   	t1 = clock() - t1;
+		
+   	int cost = 0;
+   	for(int i = 0; i < result->size(); i++)
+   	{
+    	PInt e = (*result)[i];
+   	    cout << e.first << "->" << e.second << endl;
         cost += matrixOfEmployeeWork[e.first][e.second];
-   }
-   cout << "Cost     : " << cost << endl;
-   cout << "Time (ms): " <<  t1 << endl;
+   	}
+   	cout << "Cost     : " << cost << endl;
+   	cout << "Time (ms): " <<  t1 << endl;
 
    return 0;
 }
